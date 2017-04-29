ffcdbea674a309d3dc5c8f103f7deae4ff2a8114
Toni Kukurin <tkukurin@gmail.com>
2017-04-29 19:28:06 +0200

    fixed calculation of relative distances

---
+++ helpers.js
@@ -0,0 +1,35 @@
+
+class WorldCoordinateHelper {
+
+    constructor(camera, containerDimensions, raycaster) {
+        this.camera = camera;
+        this.containerDimensions = containerDimensions;
+        this.raycaster = raycaster;
+    }
+
+    _getCursorPosition(clientX, clientY) {
+        var x = clientX - this.containerDimensions.left;
+        var y = clientY - this.containerDimensions.top;
+        return {x, y};
+    }
+
+
+    _toNormalizedVector(x, y) {
+        const realPos = this._getCursorPosition(x, y);
+        return new THREE.Vector2( (realPos.x / this.containerDimensions.width) * 2 - 1, 
+                                 -(realPos.y / this.containerDimensions.height) * 2 + 1);
+    }
+
+    getWorldPosition(clientX, clientY) {
+        const pos = this._toNormalizedVector(clientX, clientY);
+        this.raycaster.setFromCamera(pos, this.camera);
+        return this.raycaster.ray;
+    }
+
+    getIntersections(clientX, clientY, objects) {
+        const pos = this._toNormalizedVector(clientX, clientY);
+        this.raycaster.setFromCamera(pos, this.camera);
+
+        return this.raycaster.intersectObjects(objects);
+    }
+}
