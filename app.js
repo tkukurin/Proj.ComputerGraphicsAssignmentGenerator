70a45155a8ff08d6186988d88e289cc9bf95c74e
Toni Kukurin <tkukurin@gmail.com>
2017-05-01 12:54:21 +0200

    added index page with examples

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var data = require('./routes/data');
 
 var app = express();
@@ -21,10 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/data', data);
 
 // catch 404 and forward to error handler
