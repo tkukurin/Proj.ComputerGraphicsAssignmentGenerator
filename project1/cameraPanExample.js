f2c9e91e2dcf8cb9368c3ef6cbe2dc05241f1a28
Toni Kukurin <tkukurin@gmail.com>
2017-05-01 04:03:45 +0200

    deleted unnecessary files

--- project1/cameraPanExample.js
+++
@@ -1,9 +0,0 @@
-function panCamera(dx, dy) {
-    const current = this.camera.position;
-    this.camera.position.set(current.x + dx, current.y + dy, current.z);
-
-    vec2_centerXY.x += dx;
-    vec2_centerXY.y += dy;
-
-    this.camera.lookAt(lookVector);
-}
